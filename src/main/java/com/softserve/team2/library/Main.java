88dfd5ce8ee0f1674a9f4ea181d1d27d4533117d
MarianMilian <milanmarian@mail.ru>
2019-07-19 12:54:59 +0300

    Add Connector JDBC

--- src/main/java/com/softserve/team2/library/Main.java
+++ src/main/java/com/softserve/team2/library/Main.java
@@ -3,5 +3,13 @@ package com.softserve.team2.library;
 public class Main {
   public static void main(String[] args) {
     System.out.println("just for start...");
+    AuthorP authorP =new AuthorP();
+
+    AuthorDAO authorDAO =new AuthorDAO();
+    authorDAO.setId(15);
+
+    authorDAO.setName("Marian");
+    authorP.insertAuthor(authorDAO);
+
   }
 }
