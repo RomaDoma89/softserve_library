cc33672f988150f26930e7061a4c330c59b95a82
MarianMilian <milanmarian@mail.ru>
2019-07-21 09:59:44 +0300

    Creat Book jsp

--- src/main/java/com/softserve/team2/library/servlets/BookServlet.java
+++ src/main/java/com/softserve/team2/library/servlets/BookServlet.java
@@ -1,5 +1,32 @@
 package com.softserve.team2.library.servlets;
 
-public class BookServlet {
 
+import com.softserve.team2.library.dto.BookDto;
+import com.softserve.team2.library.services.BookService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/bookTitle")
+public class BookServlet  extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BookService bookService=new BookService();
+        String bookTitle="Effective Java";
+
+//            BookDto bookDto=bookService.findBookByTitle(bookTitle);
+        BookDto bookDto= new BookDto();
+        bookDto.setTitle("Effective Java");
+        bookDto.setAvailable(6);
+            req.setAttribute("title",bookDto);
+            req.setAttribute("ava",bookDto);
+            req.getRequestDispatcher("WEB-INF/static/availableBook.jsp").forward(req,resp);
+
+
+    }
 }
---
+++ src/main/webapp/WEB-INF/static/availableBook.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Marian
+  Date: 21.07.2019
+  Time: 9:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Available book</title>
+    <div class="title">Title: ${bookDto.title}</div>
+    <div class="available">Available ${bookDto.available}</div>
+</head>
+<body>
+
+</body>
+</html>
